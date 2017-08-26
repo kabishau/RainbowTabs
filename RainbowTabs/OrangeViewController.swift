95a9d8f2faf939ccaee4c3f39c64497aa3a7dbb0
user128830 <user128830@ny212.local>
2017-08-26 09:59:36 -0700

    creation different VC for every scene

---
+++ RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  OrangeViewController.swift
+//  RainbowTabs
+//
+//  Created by user128830 on 8/26/17.
+//  Copyright © 2017 user128830. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    
+
+    
+}
